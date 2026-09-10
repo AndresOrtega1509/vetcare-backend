64a83724e6b0d577b3b9fb0e8458e40d9b6aa851
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-09-10 13:32:23 -0500

    Se eliminan las restricciones de jakarta, para manejar las restricciones solo a nivel de datos en la clase modelo

--- src/main/java/com/devsenior/vetcare/model/Veterinario.java
+++ src/main/java/com/devsenior/vetcare/model/Veterinario.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -23,23 +22,19 @@ public class Veterinario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "El nombre del veterinario no puede estar vacío")
     @Column(length = 50, nullable = false)
     private String nombre;
 
-    @NotBlank(message = "El apellido del veterinario no puede estar vacío")
     @Column(length = 50, nullable = false)
     private String apellido;
 
     @Column(length = 20)
     private String especialidad;
 
-    @NotBlank(message = "El número de tarjeta profesional del veterinario no puede estar vacío")
     @Column(length = 20, unique = true, nullable = false)
     private String tarjetaProfesional;
 
     @OneToMany(mappedBy = "veterinario")
-    @Builder 
-    .Default
+    @Builder.Default
     private List<Cita> citas = new ArrayList<>();
 }
